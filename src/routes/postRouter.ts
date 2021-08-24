396fd92ee2abf64de739393d95f08e9662c167e4
Tiagopb <tiago000@outlook.com>
2021-08-24 15:45:06 -0300

    deploy

--- src/routes/postRouter.ts
+++ src/routes/postRouter.ts
@@ -7,7 +7,5 @@ export const postRouter = express.Router()
 
 const postController= new PostController() 
 
-
-
 postRouter.post("/create", postController.createPostController)
 postRouter.get("/:id", postController.GetPostByIdController)
